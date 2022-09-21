440a3f59e9a76a9161ef636030f9167fb2fb7f1a
Camilo Calderón Rocha <46984337+camilocallderon@users.noreply.github.com>
2022-09-20 21:59:49 -0500

    test(app): organize test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,28 +4,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:components_flutter_3/main.dart';
 
 void main() {
-  testWidgets('Find Home Screen', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('Home Page', () {
+    testWidgets('Find Home Screen View', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('Home Screen'), findsOneWidget);
-    expect(find.text('Hello World 1'), findsNothing);
+      expect(find.text('Home Screen'), findsOneWidget);
+      expect(find.text('Hello World 1'), findsNothing);
+    });
+  });
+  group('Card Page', () {
+    testWidgets('Find Card', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
 
-    await tester.tap(find.text('Card'));
-    await tester.pumpAndSettle();
+      await tester.tap(find.text('Card'));
+      await tester.pumpAndSettle();
 
-    expect(find.byKey(const Key('card-example-basic')), findsOneWidget);
-    expect(find.byType(TextButton), findsNWidgets(2));
-
-    ///expect(find.text('1'), findsNothing);
-    ///
-    ///// Tap the '+' icon and trigger a frame.
-    ///await tester.tap(find.byIcon(Icons.add));
-    ///await tester.pump();
-    ///
-    ///// Verify that our counter has incremented.
-    ///expect(find.text('0'), findsNothing);
-    ///expect(find.text('1'), findsOneWidget);
+      expect(find.byKey(const Key('card-example-basic')), findsOneWidget);
+      expect(find.byType(TextButton), findsNWidgets(2));
+    });
   });
 }
