7ccc76b8e7e2406350a08e120850e7b8136fdf50
Camilo Calderón Rocha <46984337+camilocallderon@users.noreply.github.com>
2022-09-26 16:07:50 -0500

    test(listview 1 and 2): add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -26,4 +26,30 @@ void main() {
       expect(find.byType(TextButton), findsNWidgets(2));
     });
   });
+
+  group('List View 1', () {
+    testWidgets('Find  List Type', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
+
+      await tester.tap(find.text('ListView Tipo 1'));
+      await tester.pumpAndSettle();
+
+      expect(find.text('Megaman'), findsOneWidget);
+      expect(find.text('Metal Gear'), findsOneWidget);
+      expect(find.text('Super Smash'), findsOneWidget);
+    });
+  });
+
+  group('List View 2', () {
+    testWidgets('Find  List  Icons', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
+
+      await tester.tap(find.text('ListView Tipo 2'));
+      await tester.pumpAndSettle();
+
+      expect(find.byIcon(Icons.arrow_forward_ios_outlined), findsNWidgets(4));
+    });
+  });
 }
